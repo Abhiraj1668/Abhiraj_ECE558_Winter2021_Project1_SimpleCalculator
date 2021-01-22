551185c212a7733996734ec90ee41604c28dfa97
Abhiraj1668 <abhiraj.ae@gmail.com>
2021-01-22 05:44:08 -0800

    Layout Complete Methods Working Needs Warning for invalid inputs

--- app/src/main/java/ece558/Project1/abhiraj_project1/MainActivity.java
+++ app/src/main/java/ece558/Project1/abhiraj_project1/MainActivity.java
@@ -3,6 +3,11 @@ package ece558.Project1.abhiraj_project1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doAddition(View view){
+        EditText op1 = findViewById(R.id.editText_op1);
+        EditText op2 = findViewById(R.id.editText_op2);
+        TextView Result = findViewById(R.id.finalResult);
+        Double res = Double.parseDouble(op1.getText().toString())+Double.parseDouble(op2.getText().toString());
+        Result.setText(Double.toString(res));
+    }
+
+    public void doSubstract(View view){
+        EditText op1 = findViewById(R.id.editText_op1);
+        EditText op2 = findViewById(R.id.editText_op2);
+        TextView Result = findViewById(R.id.finalResult);
+        Double res = Double.parseDouble(op1.getText().toString())-Double.parseDouble(op2.getText().toString());
+        Result.setText(Double.toString(res));
+    }
+
+    public void doMultiplication(View view){
+        EditText op1 = findViewById(R.id.editText_op1);
+        EditText op2 = findViewById(R.id.editText_op2);
+        TextView Result = findViewById(R.id.finalResult);
+        Double res = Double.parseDouble(op1.getText().toString())*Double.parseDouble(op2.getText().toString());
+        Result.setText(Double.toString(res));
+    }
+
+    public void doDivision(View view){
+        EditText op1 = findViewById(R.id.editText_op1);
+        EditText op2 = findViewById(R.id.editText_op2);
+        TextView Result = findViewById(R.id.finalResult);
+        Double res = Double.parseDouble(op1.getText().toString())/Double.parseDouble(op2.getText().toString());
+        Result.setText(Double.toString(res));
+    }
+
+    public void doPercent(View view){
+        EditText op1 = findViewById(R.id.editText_op1);
+        EditText op2 = findViewById(R.id.editText_op2);
+        TextView Result = findViewById(R.id.finalResult);
+        Double res = Double.parseDouble(op1.getText().toString())/100.00;
+        Result.setText(Double.toString(res));
+    }
+
+    public void doSQRT(View view){
+        EditText op1 = findViewById(R.id.editText_op1);
+        EditText op2 = findViewById(R.id.editText_op2);
+        TextView Result = findViewById(R.id.finalResult);
+        Double res = Math.sqrt(Double.parseDouble(op1.getText().toString()));
+        Result.setText(Double.toString(res));
+
     }
 }
